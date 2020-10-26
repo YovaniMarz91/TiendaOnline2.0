76cf5d080728b43d866e8e339ef3d12eafa6c0fb
kathylopez133 <kathylopez3316@gmail.com>
2020-10-25 20:57:34 -0600

    Tienda

---
+++ app/src/main/java/com/example/tiendaonline20/ConexionCouch.java
@@ -0,0 +1,6 @@
+package com.example.tiendaonline20;
+
+public class ConexionCouch {
+    static String url_consulta = "http://192.168.1.12:5984/db_productos/_desing/producto/_view/mi-productos/";
+    static  String url_mto = "http://192.168.1.12:5984/db_productos/";
+}
